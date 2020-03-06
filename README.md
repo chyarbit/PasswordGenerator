695619c5610e70dd791e3997dc1c48c9b5d8d759
Cynthia Yuen <cynthia.h.yuen@gmail.com>
2020-03-05 20:47:37 -0600

    final version

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # PasswordGenerator
+
+This is a password generator that demonstrates the understanding of HTML,  and Javascript concepts.
+
+It consists of an .html file (index.html) and a .js file (script.js) that will allow the user to enter in a response to designated questions which act as preferences in order to generate a random password.
+
+References:
+
+In order to generate random alphabet letters, I utilitized the charset string that will automatically convert the random number into the corresponding letter.  This link (http://www.net-comber.com/charset.html) showed me what the values were so that I can correctly adjust my formula.   
+
+Links-
+Github repository link: https://github.com/chyarbit/PasswordGenerator 
+
+Password Generator direct link: https://chyarbit.github.io/PasswordGenerator/
--- script.js
+++ script.js
@@ -1,138 +1,94 @@
-//Define variables and arrays to be used
-    //Define variable stringLength to be 12 characters
-    var stringLength = "12";
-        console.log(stringLength);
+//Password Length Ask
+var passwordAlert = prompt("In order to generate your password, please enter a number between 8 and 128.");
+    console.log(passwordAlert);
+    // If user enters in number greater than 8 or greater than 128
+    if (passwordAlert >= 8 || passwordAlert <= 128) {
+        alert("Your entry meets the required character allotment. Let's move on.")
+    }
+ // *I am not sure why the else if isn't working?  It was working when I switched the order earlier*   
+    else if (passwordAlert < 8 || passwordAlert > 128){
+        alert("Your entry did not meet the required character allotment.  Please try again.");
+    //insert loop back to passwordAlert if user doesn't enter in a valid number
+    }
 
-    // Define variable upperOptions to be an array of A-Z (string)
-    var upperOptions = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        console.log(upperOptions);
+//Define variable called passwordLength
+var passwordLength = [passwordAlert];
+    console.log(passwordLength);
 
-    // Define variable lowerOptions to be an array of a-z (string)
-    var lowerOptions = "abcdefghijklmnopqrstuvwxyz";
-        console.log(lowerOptions);
-
-    // Define variable numOptions to an array of 0-9 (string)
-    var numOptions ="0123456789";
-        console.log(numOptions);
-
-    // Define variable spCharOptions to be an array of available special character options (string)
-    var spCharOptions ="!@#$%^&*";  
-        console.log(spCharOptions);
-
-    // Define variable allOptions to include all variables
-        var allOptions = upperOptions + lowerOptions + numOptions + spCharOptions;
-        console.log(allOptions);
-    
-    // Define variable password
-        var password = "";
-
-// Initial Asks
-    //Password Length Alert
-    var passwordAlert = alert("Your randomly generated password will be 12 characters.");
-    
     //Uppercase Letters Confirmation
-    var confirmUpper = confirm("Do you want any uppercase letters in your password?");
+var confirmUpper = confirm("Do you want any uppercase letters in your password?");
+    // If user confirms that they want upper letters in their password,
+    if (confirmUpper) {
+        // Define a function named getRandomUpper to get a random uppercase letter
+        function getRandomUpper () {
+        // This will return a random uppercase letter based on a string calling a predetermined set of values that have been paired to a number.  There are 26 options and the uppercase letters start at a value of 65.
+        return String.fromCharCode(Math.floor(Math.random()*26)+65);
+        }
+    };
+    console.log(getRandomUpper());
 
     //Lowercase Letters Confirmation
-    var confirmLower = confirm("Do you want any lowercase letters in your password?");
-
-    // Numbers Confirmation
-    var confirmNumber = confirm("Do you want any numbers in your password?");
-
-    // Special Characters Confirmation 
-    var confirmSpecial = confirm("Do you want any special characters in your password?");
-
-if (confirmUpper) {
-    function generate (stringLength = 12) {
-        for (var i = 0; i < stringLength; i++){
-            var character = Math.floor(Math.random()* allOptions.length);
-            password += allOptions.substring(character, character + 1);
-            console.log(password);
+var confirmLower = confirm("Do you want any lowercase letters in your password?");
+    // If user confirms that they want lower letters in their password, 
+    if (confirmLower) {
+        // Define a function named getRandomLower to get a random lowercase letter
+        function getRandomLower () {
+        // This will return a random lowercase letter based on a string calling a predetermined set of values that have been paired to a number.  There are 26 options and the lowercase letters start at a value of 97.
+        return String.fromCharCode(Math.floor(Math.random()*26)+97);
         }
+    };
+    console.log(getRandomLower());
+
+// Numbers Section
+    // Define variable numOptions to an array of 0-9 (string)    
+    var numOptions ="0123456789";
+    // Number confirmation
+    var confirmNumber = confirm("Do you want any numbers in your password?");
+    // If user confirms that they want numbers in their password,
+    if (confirmNumber) {
+        var getRandomNumber= Math.floor(Math.random()*numOptions.length)
+    };
+    console.log(getRandomNumber);
+
+// Special Characters Section 
+    // Define variable spCharOptions to be an array of available special character options (string)
+    const spCharOptions ="!@#$%^&*"; 
+    // Special Character confirmation
+    var confirmSpecial = confirm("Do you want any special characters in your password?");
+    // If user confirms that they want special characters in their password,
+    if (confirmSpecial){
+    function getRandomSpecial(){
+        return spCharOptions[Math.floor(Math.random()*spCharOptions.length)];
+        }
+    };
+    console.log(getRandomSpecial());
+
+// Define variable finalPassword 
+var finalPassword = ["getRandomUpper", "GetRandomLower", "getRandomNumber", "getRandomSpecial"];
+    console.log(finalPassword)
+
+//**I don't really understand anything else after this.  I can get a loop to go by each random element if I wanted to program them individually, but I can't when I combine all the components into a variable.  The array is then 4, but I set it to the password length and then tried to randomize the outcome to get the final password */
+
+// Create a function called generatePassword which will loop for the length that the user defined in passwordLength
+function generatePassword(){
+    for (var i=0; i < passwordLength; i++) {
+    // Randomize the array of characters that are generated
+    Math.random(finalPassword*passwordLength)
     }
 }
-
-
-// If user confirms that they want upper letters in their password,
-//if (confirmUpper) {
-    //for (var i=0; i < 12; i++) {
-    // randomly generate upper letters
-    //var randomUpper= Math.floor(Math.random()*upperOptions.length)
-        //console.log(randomUpper);
-    //}
-//}
-
-// If user confirms that they want lower letters in their password, 
-//if (confirmLower) {
-    //for (var i=0; i < 12; i++) {
-        // Generate random lower letters
-        //var randomLower= Math.floor(Math.random()*lowerOptions.length)
-            //console.log(randomLower);
-        //}
-//}
-
-// If user confirms that they want numbers in their password,
-//if (confirmNumber) {
-    //for (var i=0; i < 12; i++) {
-        // Generate random number
-        //var randomNumber= Math.floor(Math.random()*numOptions.length)
-            //console.log(randomNumber);
-        //}
-//}
-
-// If user confirms that they want special characters in their password,
-//if (confirmSpecial) {
-    
-    //for (var i=0; i < 12; i++) {
-        // Generate random special character
-        //var randomSpecial= Math.floor(Math.random()*spCharOptions.length)
-            //console.log(randomSpecial);
-        //}
-
-//}
-
-//if (!confirmUpper && !confirmLower && !confirmNumber && !confirmSpecial) {
-           //Loop for ?
-           //for (var i = 0; i < stringLength ; i++) {
-            // Generate random ?
-           // var randomUpper = Math.round(Math.random() * 10)  
-            //console.log("randomUpper" + i); 
-    //}
-//}
-
-// array[].push (custom function)
-//if 
-    //if (confirmLower) {
-        //function of all characters for the password
-        //nested inside are all the different types 
-        // Loop 26 times for lowercase letters a to z
-        //for (var i = 0; i < 27; i++) {
-         // Generate random letter between a and z
-         //var randomLower = Math.round(Math.random() * 10)  
-         //console.log("LowerCase" + i); 
- //}
-//}
-
-    //if (confirmNumbers) {
-        // Loop 10 times for numbers 0-9
-        //for (var i = 0; i < 10; i++) {
-         // Generate random number between 0 and 9
-         //var randomNumbers = Math.round(Math.random() * 10)  
-         //console.log("Number" + i); 
- //}
-//}
+    console.log(generatePassword)
 
 // Assignment Code
-//var generateBtn = document.querySelector("#generate").textcontent = "<insert the random password>";
+var generateBtn = document.querySelector("#generate").textcontent = "<insert the random password>";
 
 // Write password to the #password input
-//function writePassword(<insert paramater?>) {
-  //var password = generatePassword();
-  //var passwordText = document.querySelector("#password");
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
   
-  //passwordText.value = password;
+  passwordText.value = password;
 
-//}
+}
 
 // Add event listener to generate button
-//generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword);
